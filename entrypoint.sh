c269c3da30657465ebb9b9d4f5824ee20711e371
airbone95 <airbone95@users.noreply.github.com>
2022-05-18 11:10:14 +0200

    improved output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,47 @@
 #!/bin/ash
 
 TMPFILE=$(mktemp)
+TMPFILE2=$(mktemp)
+
+e() {
+  rm $TMPFILE $TMPFILE2
+  exit $1
+}
 
 ## retrieve token from dockerhub to do operations
-TOKEN=$(curl -sL -u "$INPUT_DOCKERHUB_USERNAME:$INPUT_DOCKERHUB_PASSWORD" "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$INPUT_DOCKERHUB_REPO:pull,push" | jq --raw-output .token)
+echo "retrieving token from https://auth.docker.io/token?service=registry.docker.io&scope=repository:$INPUT_DOCKERHUB_REPO:pull,push"
+TOKEN=$(curl -sL -u "$INPUT_DOCKERHUB_USERNAME:$INPUT_DOCKERHUB_PASSWORD" "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$INPUT_DOCKERHUB_REPO:pull,push" | tee $TMPFILE | jq --raw-output .token)
+if [ -z "$TOKEN" ]; then
+  echo "error recieving token:"
+  cat $TMPFILE
+  e 1
+else
+  echo "token recieved"
+fi
 
 ## retrieve manifest of docker image
-curl -sL -H "Authorization: Bearer $TOKEN" -H "Accept: application/vnd.docker.distribution.manifest.v2+json"  "https://index.docker.io/v2/$INPUT_DOCKERHUB_REPO/manifests/$INPUT_OLD_TAG" > $TMPFILE
+echo "retrieving manifest from https://index.docker.io/v2/$INPUT_DOCKERHUB_REPO/manifests/$INPUT_OLD_TAG"
+RC=$(curl -sL -H "Authorization: Bearer $TOKEN" -H "Accept: application/vnd.docker.distribution.manifest.v2+json"  "https://index.docker.io/v2/$INPUT_DOCKERHUB_REPO/manifests/$INPUT_OLD_TAG" -o $TMPFILE -w "%{http_code}")
+if [ "$RC" -eq 200 ]; then
+  echo "manifest for $INPUT_OLD_TAG retrieved successfully"
+else
+  echo "error retrieving manifest for $INPUT_OLD_TAG:"
+  cat $TMPFILE
+  e 2
+fi
 
 ## push manifest with new tag to dockerhub
-curl -sL -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/vnd.docker.distribution.manifest.v2+json"  "https://registry-1.docker.io/v2/$INPUT_DOCKERHUB_REPO/manifests/$INPUT_NEW_TAG" -X PUT -d "@$TMPFILE"
+echo "pushing manifest to https://registry-1.docker.io/v2/$INPUT_DOCKERHUB_REPO/manifests/$INPUT_NEW_TAG"
+RC=$(curl -sL -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/vnd.docker.distribution.manifest.v2+json"  "https://registry-1.docker.io/v2/$INPUT_DOCKERHUB_REPO/manifests/$INPUT_NEW_TAG" -X PUT -d "@$TMPFILE" -o $TMPFILE2 -w "%{http_code}")
+if [ "$RC" -eq 201 ]; then
+  echo "new tag $INPUT_NEW_TAG created successfully"
+else
+  echo "error creating new tag $INPUT_NEW_TAG:"
+  cat $TMPFILE2
+  e 3
+fi
+
 
 ## cleanup
-rm $TMPFILE
+rm $TMPFILE $TMPFILE2
+exit 0
